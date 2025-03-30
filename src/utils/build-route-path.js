9ecdab43e9541e96d8d90967464cfadd4c10af6f
Yann <yanngames68@gmail.com>
2025-03-30 00:39:56 -0300

    New routes

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -1,8 +1,9 @@
 export function buildRoutePath(path) {
+  // Pegar da rota tudo que começe com :
   const routeParametersRegex = /:([a-zA-Z]+)/g
-  const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  // ex: substituir a nossa o rota o /:id pelo id enviado do usuário: (7cddb688-008e-4e90-86e3-c5a54e10a542)
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)') //$1 cria um grupo com nome id
 
-  return pathRegex
+  return new RegExp(`^${pathWithParams}`)
 }
