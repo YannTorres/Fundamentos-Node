1352709db2b33469f35d7aa6dea5109ead20c8a1
Yann <yanngames68@gmail.com>
2025-03-30 21:03:55 -0300

    Add Query Filter

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,8 +2,8 @@ export function buildRoutePath(path) {
   // Pegar da rota tudo que começe com :
   const routeParametersRegex = /:([a-zA-Z]+)/g
 
-  // ex: substituir a nossa o rota o /:id pelo id enviado do usuário: (7cddb688-008e-4e90-86e3-c5a54e10a542)
+  // Ex: substituir a nossa o rota o /:id pelo id enviado do usuário: (7cddb688-008e-4e90-86e3-c5a54e10a542)
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)') //$1 cria um grupo com nome id
 
-  return new RegExp(`^${pathWithParams}`)
+  return new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 }
